bb42510845fe97855c49a353133ae9b7901f5a54
Dawid Dahl <dawiddahl@gmail.com>
2021-02-22 12:57:20 +0100

    fixed many-to-many relationship

--- api/models.py
+++ api/models.py
@@ -18,10 +18,10 @@ class Product(models.Model):
     ]
     product_type = models.CharField(max_length=30, choices=PRODUCT_TYPES)
     title = models.CharField(max_length=50)
-    image = models.CharField(max_length=200)
+    image = models.CharField(max_length=200, null=True)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10000, decimal_places=2)
-    ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- api/views.py
+++ api/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from .models import Product, Ingredient
 from .serializers import ProductSerializer, IngredientSerializer
 from rest_framework import generics, serializers
