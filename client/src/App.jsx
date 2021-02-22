114e14d52051240edfaf22c7011f82dd4cd2b785
Dawid Dahl <dawiddahl@gmail.com>
2021-02-22 18:30:56 +0100

    set upp styled-components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import { Test } from "./components/Test";
+import {TestComp} from "./components/TestComp";
+import styled from "styled-components";
 
 const App = () => {
-    return (
-        <>
-            <Switch>
-                <Route exact path="/" component={Test} />
-            </Switch>
-        </>
-    );
+	return (
+		<Root>
+			<Switch>
+				<Route exact path="/" component={TestComp} />
+			</Switch>
+		</Root>
+	);
 };
 
+const Root = styled.div``;
+
 ReactDOM.render(
-        <Router>
-            <Route path="/" component={App} />
-        </Router>,
-    document.getElementById("root")
+	<Router>
+		<Route path="/" component={App} />
+	</Router>,
+	document.getElementById("root")
 );
+
+export default App;
