0c297aac0e96ebcd9bee78d9a3cf8d7dc6043fc0
Dawid Dahl <dawiddahl@gmail.com>
2021-02-25 23:29:24 +0100

    started separating frontend from backend

--- stereo_nightclub/settings.py
+++ stereo_nightclub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'w@glgu#swd^$4^w7t!&)sdwj+7dm@16b+zvt$f^j#36qvj6-zk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stereo-nightclub.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['stereo-nightclub.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
